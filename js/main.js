2f76bb65074c3790c79eb633b130039ae703a49a
holgermueller <9jholger@gmail.com>
2024-02-12 16:03:32 -0600

    converted csv file to json

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 "use strict";
+const fs = require("fs");
+const csvFile = fs.readFileSync("./city_coordinates.csv");
+const arr = csvFile.toString().split("\n");
+// console.log(arr);
+let jsonObject = [];
+let headers = arr[0].split(",");
+// console.log(headers);
+for (let i = 1; i < arr.length; i++) {
+  let data = arr[i].split(",");
+  let object = {};
+  console.log(data);
+  for (let j = 0; j < data.length; j++) {
+    object[headers[j].trim()] = data[j].trim();
+  }
+  jsonObject.push(object);
+}
+console.log(JSON.stringify(jsonObject));
