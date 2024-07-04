46293db00da7957da58d409ba0de3100fedbbafb
holgermueller <9jholger@gmail.com>
2024-07-04 17:20:13 -0500

    got most of the weather data on the dom

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 "use strict";
 
-// Convert CSV to JSON
+let weatherBlock;
+const weatherDisplay = document.getElementById("weatherDisplay");
+
 async function createSelector() {
   await fetch("../city_coordinates.csv")
     .then((response) => {
@@ -39,20 +41,148 @@ let selectedCity = document.getElementById("citySelector");
 selectedCity.onchange = function (event) {
   let latitude = event.target.options[event.target.selectedIndex].dataset.lat;
   let longitude = event.target.options[event.target.selectedIndex].dataset.long;
-  console.log(event.target.options[event.target.selectedIndex].dataset.lat);
+
+  if (weatherBlock) {
+    resetWeatherDisplay();
+  }
 
   fetch(
-    "http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=civillight&output=json"
+    "http://www.7timer.info/bin/api.pl?lon=" +
+      longitude +
+      "&lat=" +
+      latitude +
+      "&product=civillight&output=json"
   )
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data.dataseries);
+
+      let allData = data.dataseries;
+      allData.forEach((datum) => {
+        let dateDisplay = document.createElement("p");
+        dateDisplay.classList.add("date");
+        dateDisplay.innerHTML = datum.date;
+
+        let iconDisplay = document.createElement("img");
+        iconDisplay.classList.add("icon-display");
+
+        let singleWeatherDisplay = document.createElement("p");
+        singleWeatherDisplay.classList.add("weather");
+        let weather = datum.weather;
+
+        let correctedWeather;
+        if (weather === "ishower") {
+          correctedWeather = "Isolated Showers";
+        } else if (weather === "lightrain") {
+          correctedWeather = "Light Rain";
+        } else if (weather === "lightsnow") {
+          correctedWeather = "Light Snow";
+        } else if (weather === "mcloudy") {
+          correctedWeather = "Mostly Cloudy";
+        } else if (weather === "oshower") {
+          correctedWeather = "Occassional Showers";
+        } else if (weather === "pcloudy") {
+          correctedWeather = "Partly Cloudy";
+        } else if (weather === "rainshow") {
+          correctedWeather = "Mix of Rain and Snow";
+        } else if (weather === "tsrain") {
+          correctedWeather = "Thunderstorms Possible";
+        } else if (weather === "tstorm") {
+          correctedWeather = "Thunderstorms";
+        } else {
+          correctedWeather = weather.charAt(0).toUpperCase() + weather.slice(1);
+        }
+
+        singleWeatherDisplay.innerHTML = correctedWeather;
+
+        if (weather === "clear") {
+          iconDisplay.src = "./images/clear.png";
+        }
+
+        if (weather === "cloudy") {
+          iconDisplay.src = "./images/cloudy.png";
+        }
+
+        if (weather === "fog") {
+          iconDisplay.src = "./images/fog.png";
+        }
+
+        if (weather === "humid") {
+          iconDisplay.src = "./images/humid.png";
+        }
+
+        if (weather === "ishower") {
+          iconDisplay.src = "./images/ishower.png";
+        }
+
+        if (weather === "lightrain") {
+          iconDisplay.src = "./images/lightrain.png";
+        }
+
+        if (weather === "lightsnow") {
+          iconDisplay.src = "./images/lightsnow.png";
+        }
+
+        if (weather === "mcloudy") {
+          iconDisplay.src = "./images/mcloudy.png";
+        }
+
+        if (weather === "oshower") {
+          iconDisplay.src = "./images/oshower.png";
+        }
+
+        if (weather === "pcloudy") {
+          iconDisplay.src = "./images/pcloudy.png";
+        }
+
+        if (weather === "rain") {
+          iconDisplay.src = "./images/rain.png";
+        }
+
+        if (weather === "rainsnow") {
+          iconDisplay.src = "./images/rainsnow.png";
+        }
+
+        if (weather === "snow") {
+          iconDisplay.src = "./images/snow.png";
+        }
+
+        if (weather === "tsrain") {
+          iconDisplay.src = "./images/tsrain.png";
+        }
+
+        if (weather === "tstorm") {
+          iconDisplay.src = "./images/tstorm.png";
+        }
+
+        if (weather === "windy") {
+          iconDisplay.src = "./images/windy.png";
+        }
+
+        let tempDisplay = document.createElement("p");
+        tempDisplay.classList.add("temps");
+        tempDisplay.innerHTML =
+          datum.temp2m.min + "&deg;/" + datum.temp2m.max + "&deg;";
+
+        weatherBlock = document.createElement("div");
+        weatherBlock.classList.add("weather-block");
+        weatherBlock.appendChild(dateDisplay);
+        weatherBlock.appendChild(iconDisplay);
+        weatherBlock.appendChild(singleWeatherDisplay);
+        weatherBlock.appendChild(tempDisplay);
+
+        weatherDisplay.appendChild(weatherBlock);
+      });
     })
     .catch((err) => {
       console.warn(err);
     });
+};
+
+const resetWeatherDisplay = () => {
+  if (weatherBlock) weatherDisplay.replaceChildren(weatherBlock);
 };
 
 createSelector();
